95c3085266eda51e2f98659bc78bd1a3bf563aa2
LordOfTheDeadlines <49224188+LordOfTheDeadlines@users.noreply.github.com>
2020-12-06 20:27:11 +0500

    fix post-requests

--- src/main/java/com/lod/JuniorLib/config/WebSecurityConfig.java
+++ src/main/java/com/lod/JuniorLib/config/WebSecurityConfig.java
@@ -40,7 +40,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
                 .passwordEncoder(NoOpPasswordEncoder.getInstance());
-//                .usersByUsernameQuery("select username, password, active from usr where username=?")
-//                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id=ur.user_id where username=?");
-    }
+ }
 }
